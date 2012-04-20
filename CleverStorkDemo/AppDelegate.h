89bd7031f09798946bab3dc6b26868746f8ab3a0
Sunil Gowda <sunil@slykr.com>
2012-04-19 18:56:28 -0700

    shell project

---
+++ CleverStorkDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CleverStorkDemo
+//
+//  Created by Sunil Gowda on 4/19/12.
+//  Copyright (c) 2012 Slykr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
